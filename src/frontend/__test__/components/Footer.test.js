6c505aa6e420776220c6a01f6ee51eaba6bd7c0a
Jose Cuevas <josecueram@gmail.com>
2020-05-29 12:20:49 -0500

    added snapshot  to footer

--- src/frontend/__test__/components/Footer.test.js
+++ src/frontend/__test__/components/Footer.test.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import { mount } from 'enzyme'
 import Footer from '../../components/Footer'
+import { create } from 'react-test-renderer'
 
 describe('<Footer /> Tests', () => {
   const footer = mount(<Footer />)
@@ -11,6 +12,11 @@ describe('<Footer /> Tests', () => {
 
   test('Footer has 3 anchors', () => {
     expect(footer.find('a')).toHaveLength(3)
+  })
+
+  test('Footer Snapshot', () => {
+    const footer = create(<Footer />)
+    expect(footer.toJSON()).toMatchSnapshot()
   })
 
 })
