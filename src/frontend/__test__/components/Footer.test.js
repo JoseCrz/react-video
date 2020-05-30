041c93bddb704d594a0a2d6034aa2f4da759f851
Jose Cuevas <josecueram@gmail.com>
2020-05-29 20:09:10 -0500

    updated footer component & it's tests

--- src/frontend/__test__/components/Footer.test.js
+++ src/frontend/__test__/components/Footer.test.js
@@ -2,9 +2,14 @@ import React from 'react'
 import { mount } from 'enzyme'
 import Footer from '../../components/Footer'
 import { create } from 'react-test-renderer'
+import ProviderMock from '../../__mocks__/providerMock'
 
 describe('<Footer /> Tests', () => {
-  const footer = mount(<Footer />)
+  const footer = mount(
+    <ProviderMock>
+      <Footer />
+    </ProviderMock>
+  )
 
   test('Component Rendering', () => {
     expect(footer.length).toEqual(1)
@@ -15,7 +20,11 @@ describe('<Footer /> Tests', () => {
   })
 
   test('Footer Snapshot', () => {
-    const footer = create(<Footer />)
+    const footer = create(
+      <ProviderMock>
+        <Footer />
+      </ProviderMock>
+    )
     expect(footer.toJSON()).toMatchSnapshot()
   })
 
--- src/frontend/components/Footer.jsx
+++ src/frontend/components/Footer.jsx
@@ -6,9 +6,9 @@ import '../assets/sass/components/Footer.scss'
 const Footer = () => {
   return (
     <footer className="Footer">
-      <a to="/" className="Footer__link">Terms & Conditions</a>
-      <a to="/" className="Footer__link">Privacy</a>
-      <a to="/" className="Footer__link">Help Center</a>
+      <Link to="/" className="Footer__link">Terms & Conditions</Link>
+      <Link to="/" className="Footer__link">Privacy</Link>
+      <Link to="/" className="Footer__link">Help Center</Link>
     </footer>
   )
 }
